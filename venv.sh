5840b6380cb40d1757565fb6cb0c61008dbcc87e
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-21 21:43:00 +0900

    Change requirements.txt, stop.sh and venv.sh.

--- venv.sh
+++ venv.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-virtualenv venv
+if type virtualenv-2 > /dev/null 2>&1 ; then
+	VIRTUALENV=virtualenv-2
+elif type virtualenv2 > /dev/null 2>&1 ; then
+	VIRTUALENV=virtualenv2
+elif type virtualenv > /dev/null 2>&1 ; then
+	VIRTUALENV=virtualenv
+else
+	echo 'Your platform is not supported : no virtualenv'
+	exit 1
+fi
+
+$VIRTUALENV venv
 . venv/bin/activate
 pip install -r requirements.txt
